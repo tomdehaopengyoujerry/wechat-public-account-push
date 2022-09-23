ccf2eb178aea9f384054fe9a9f149a128ecae364
tomdehaopengyoujerry <114204583+tomdehaopengyoujerry@users.noreply.github.com>
2022-09-24 00:42:38 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -7,6 +7,6 @@ export const USER_CONFIG = {
   TEMPLATE_ID:"MN0WuoPxB22DJI8iG4lxW82uuwuBwKQ2MTtBsDYPihQ",
   USERS:["o4NJj6kxtb_iFP6jnI-rm05YQBvo","o4NJj6kgsctaIXaxTawTF2L2v6s4","o4NJj6pLMJKlq8u4YcRm9X0-OIGc","o4NJj6kRUQeIYF9FQxBQnZ-AxxWk"],
   PROVINCE: "四川",
-  CITY: "成都",
+  CITY: "成都"
 }
 
