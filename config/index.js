61ad37259ba4dc446b65392f4ede676a61ac27a2
tomdehaopengyoujerry <114204583+tomdehaopengyoujerry@users.noreply.github.com>
2022-09-23 15:54:11 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -2,16 +2,9 @@ export const CONFIG = {
   /**
    * 公众号配置
    */
-  
-  // 公众号APP_ID
-  // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
-  // 如果你非要填这里也行。脚本也能运行
   APP_ID: "wx339b603409f69991",
-  
-  // 公众号APP_SECRET
-  // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
-  // 如果你非要填这里也行。脚本也能运行
   APP_SECRET: "6ffe2254ba1e912f9667550c775a2793",
+  TEMPLATE_ID:"b1qc8laHm1B98wnHVW3lRnOQQNKDm_XZJEGQKHldwTI",
   USERS:["o4NJj6kxtb_iFP6jnI-rm05YQBvo","o4NJj6kgsctaIXaxTawTF2L2v6s4","o4NJj6pLMJKlq8u4YcRm9X0-OIGc","o4NJj6kRUQeIYF9FQxBQnZ-AxxWk"],
   province: "四川",
   city: "成都"
