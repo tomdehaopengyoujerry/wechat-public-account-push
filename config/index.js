b5943c8f211be7a03242286db7381d7516d36df7
tomdehaopengyoujerry <114204583+tomdehaopengyoujerry@users.noreply.github.com>
2022-09-24 00:53:13 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -4,7 +4,7 @@ export const USER_CONFIG = {
    */
   APP_ID: "wx339b603409f69991",
   APP_SECRET: "34603fad041d32192a132b8bd334d52a",
-  TEMPLATE_ID:"MN0WuoPxB22DJI8iG4lxW82uuwuBwKQ2MTtBsDYPihQ",
+  useTemplateId:"MN0WuoPxB22DJI8iG4lxW82uuwuBwKQ2MTtBsDYPihQ",
   USERS:["o4NJj6kxtb_iFP6jnI-rm05YQBvo","o4NJj6kgsctaIXaxTawTF2L2v6s4","o4NJj6pLMJKlq8u4YcRm9X0-OIGc","o4NJj6kRUQeIYF9FQxBQnZ-AxxWk"],
   PROVINCE: "四川",
   CITY: "成都"
